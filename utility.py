39094b278047e8954a56ff5172565c5d48ac21e5
Llily Ella C <lilyellacallard@gmail.com>
2026-01-05 17:27:15 -0500

    more stuff, including images of the crossword

--- utility.py
+++ utility.py
@@ -1,4 +1,6 @@
 import constants as const
+import pygame
+pygame.init()
 
 #helpful pushing text to screen function
 def toScreen(words, font, colour, x, y):
@@ -8,7 +10,7 @@ def toScreen(words, font, colour, x, y):
     const.screen.blit(text, textRect)
 
 #versions to push more than 1 line
-def toScreen2(words1, words2, font, colour, x, y, screen):
+def toScreen2(words1, words2, font, colour, x, y):
     toScreen(words1, font, colour, x, y-font.get_height()//2)
     toScreen(words2, font, colour, x, y+font.get_height()//2)
 
@@ -16,3 +18,8 @@ def toScreen3(words1, words2, words3, font, colour, x, y):
     toScreen(words1, font, colour, x, y-font.get_height())
     toScreen(words2, font, colour, x, y)
     toScreen(words3, font, colour, x, y+font.get_height())
+
+def imageToScreen(imageName, x, y, width, height):
+    image = pygame.image.load(imageName)
+    image = pygame.transform.scale(image, (width, height))
+    const.screen.blit(image, (x,y))
